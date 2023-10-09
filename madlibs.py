6c8e974ae279b75e6c5d947bca0c2174d5cc8c53
IsaiahSantiago <IsaiahSantiago456@gmail.com>
2023-10-09 19:07:19 -0400

    Madlibs python uploaded, couldn't solve even with intructions

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+noun = pizza
+adjective = ridiculous
+adverb = those
+verb = asking
+answer = input("Say something! ")
+print( str(answer) + "on a tropical pizza'" + "Underneath a ridiculous' moon ..." + "Hanging with the those' hula dancers," + "")
+
+
